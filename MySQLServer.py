088b2bbb68b2002e72fed3a98bb2bf1e640c9f49
mahmoud nasser <126265154+kedwani@users.noreply.github.com>
2025-12-14 22:08:00 +0200

    aa

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,6 +3,8 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="kedwani-11566",
-    database="alx_book_store"
 )   
-print(mydb.get_server_info)
+mycrs = mydb.cursor()
+mycrs.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+mycrs.close()
+mydb.close()
