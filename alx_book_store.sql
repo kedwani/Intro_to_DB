6bd86c1688b9376bef812131a45b815858d8aae6
mahmoud nasser <126265154+kedwani@users.noreply.github.com>
2025-12-14 21:32:16 +0200

    a

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -16,7 +16,7 @@ CREATE TABLE Customers (
    customer_id INT PRIMARY KEY,
    customer_name VARCHAR(215),
    email VARCHAR(215),
-   ADDRESS TEXT
+   address TEXT
 );
 CREATE TABLE Orders (
    order_id INT PRIMARY KEY,
