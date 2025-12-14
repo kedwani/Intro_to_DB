2dfbe1997fa1f6235c09340362fa8107d17553c2
mahmoud nasser <126265154+kedwani@users.noreply.github.com>
2025-12-14 22:09:25 +0200

    a

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,6 +4,10 @@ mydb = mysql.connector.connect(
     user="root",
     password="kedwani-11566",
 )   
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+    
 mycrs = mydb.cursor()
 mycrs.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 mycrs.close()
